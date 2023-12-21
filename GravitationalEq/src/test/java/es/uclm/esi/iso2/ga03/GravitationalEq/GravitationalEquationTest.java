dbea1043dc6e75cbc2fb7443158d6f74f7064cf5
Manueehh <72602215+Manueehh@users.noreply.github.com>
2023-12-21 18:09:43 +0100

    Add files via upload

---
+++ GravitationalEq/src/test/java/es/uclm/esi/iso2/ga03/GravitationalEq/GravitationalEquationTest.java
@@ -0,0 +1,19 @@
+package es.uclm.esi.iso2.ga03.GravitationalEq;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GravitationalEquationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
